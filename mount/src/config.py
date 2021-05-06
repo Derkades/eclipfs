49cd1f2293ecb599d61e8a5df2e1dfd52840c629
Robin <robinslot@hotmail.nl>
2021-05-06 12:10:55 +0200

    config explanation

--- mount/src/config.py
+++ mount/src/config.py
@@ -21,8 +21,14 @@ REQUEST_BACKOFF_FACTOR = 0.1
 REQUEST_BACKOFF_MAX = 1
 
 MAX_WRITE_BUFFER_SIZE = 5
+
+# Max time to keep entries in read cache. Note that the read cache doesn't have
+# a maximum size. For example, if you transfer files at 20MB/s with READ_CACHE_TIME=30
+# it will use ~600MB of system memory. Python may use way more, but this
+# is memory that will be garbage collected when the system needs it.
 READ_CACHE_TIME = 30
-# Will improve performance from ~5MB/s to ~20MB/s but can affect system stability when the filesystem does
+
+# Will improve write throughput from ~5MB/s to ~20MB/s but can affect system stability when the filesystem does
 # not respond. Only enable if "writing" to the filesystem is the bottleneck instead of uploading over the network.
 ENABLE_WRITEBACK_CACHE = False
 
