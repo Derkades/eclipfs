6967aa68779a327ae48180951a3a6a2931154362
Robin <robinslot@hotmail.nl>
2021-06-24 20:57:09 +0200

    don't build arm/arm64 for now to save on build/upload time

--- metaserver/deploy.sh
+++ metaserver/deploy.sh
@@ -2,5 +2,5 @@
 set -ex
 mvn package shade:shade
 export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx build -t eclipfs/metaserver --platform=linux/amd64,linux/arm,linux/arm64 . --push
-docker buildx build -t eclipfs/metaserver-db --platform=linux/amd64,linux/arm,linux/arm64 database --push
+docker buildx build -t eclipfs/metaserver --platform=linux/amd64 . --push
+docker buildx build -t eclipfs/metaserver-db --platform=linux/amd64 database --push
